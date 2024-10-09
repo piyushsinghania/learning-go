4bfd5eac28a4524ef2e700258a04af7efc879fd8
piyushsinghania <piyushsinghania28@gmail.com>
2024-10-09 18:28:40 +0530

    Make a post request with form data

--- 21webrequestverbs/main.go
+++ 21webrequestverbs/main.go
@@ -4,16 +4,19 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"strings"
 )
 
 const getUrl string = "http://localhost:8000/get"
 const postUrl string = "http://localhost:8000/post"
+const postFormUrl string = "http://localhost:8000/postform"
 
 func main() {
 	fmt.Println("Web request methods in go")
 	PerformGetRequest(getUrl)
 	PerformPostRequest(postUrl)
+	PerformPostFromRequest(postFormUrl)
 }
 
 func PerformGetRequest(getUrl string) {
@@ -46,6 +49,23 @@ func PerformPostRequest(postUrl string) {
 	`)
 
 	response, err := http.Post(postUrl, "application/json", requestBody)
+	checkNilErr(err)
+
+	defer response.Body.Close()
+
+	content, err := ioutil.ReadAll(response.Body)
+	checkNilErr(err)
+
+	fmt.Println(string(content))
+}
+
+func PerformPostFromRequest(postFormUrl string) {
+	data := url.Values{}
+	data.Add("firstname", "piyush")
+	data.Add("lastname", "singhania")
+	data.Add("email", "piyush@gmail.com")
+
+	response, err := http.PostForm(postFormUrl, data)
 	checkNilErr(err)
 
 	defer response.Body.Close()
